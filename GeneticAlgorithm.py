e2c08a66f4ac5ba2098ea594cd49c1d4f935d57f
Sebastian Carvalho Salazar <sebastian.carvalho2596@gmail.com>
2021-11-09 20:36:15 -0500

    Fists Commit: Genetic Algorithm in PYTHON

---
+++ GeneticAlgorithm.py
@@ -0,0 +1,61 @@
+import random
+
+modelo = [5,5,2,2,2,3,2,2,2,5,5]
+largo = 11
+num = 11
+pressure = 5
+mutation_chance = 0.5
+
+print("\n\Modelo: %s\n"%(modelo))
+
+def individual(min,max):
+    return[random.randint(min, max) for i in range(largo)]
+
+def crearPoblacion():
+    return[individual(1,9) for i in range(num)]
+
+def calcularFitness(individual):
+    fitness = 0
+    for i in range(len(individual)):
+        if individual[i] == modelo[i]:
+            fitness += 1
+
+    return fitness
+
+def selection_and_reproduction(population):
+    puntuados = [ (calcularFitness(i), i) for i in population]
+    puntuados = [i[1] for i in sorted(puntuados)]
+    population = puntuados
+
+    selected = puntuados[(len(puntuados)-pressure):]
+
+    for i in range(len(population)-pressure):
+        punto = random.randint(1,largo-1)
+        padre = random.sample(selected, 2)
+
+        population[i][:punto] = padre[0][:punto]
+        population[i][punto:] = padre[1][punto:]
+
+    return population
+
+def mutation(population):
+    for i in range(len(population)-pressure):
+        if random.random() <= mutation_chance:
+            punto = random.randint(0,largo-1)
+            nuevo_valor = random.randint(1,9)
+
+            while nuevo_valor == population[i][punto]:
+                nuevo_valor = random.randint(1,9)
+            population[i][punto] = nuevo_valor
+
+    return population
+
+population = crearPoblacion()
+print("Población Inicial:\n%s"%(population))
+
+for i in range(100):
+    population = selection_and_reproduction(population)
+    population = mutation(population)
+
+print("\nPoblación Final:\n%s"%(population))
+print("\n\n")
